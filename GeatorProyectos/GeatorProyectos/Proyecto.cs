30c7209c3d455d604ac1131bcfb02540efc7fb86
santidebosis <santidebosis08@gmail.com>
2024-10-24 22:21:37 -0300

    cambio

---
+++ GeatorProyectos/GeatorProyectos/Proyecto.cs
@@ -0,0 +1,26 @@
+﻿using GeatorProyectos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public abstract class Proyecto
+{
+    public string Nombre { get; set; }
+    public EstadoProyecto Estado { get; set; }
+    public int CantidadDesarrolladores { get; set; }
+    public DateTime FechaInicio { get; set; }
+
+    public Proyecto(string nombre, EstadoProyecto estado, int cantidadDesarrolladores, DateTime fechaInicio)
+    {
+        Nombre = nombre;
+        Estado = estado;
+        CantidadDesarrolladores = cantidadDesarrolladores;
+        FechaInicio = fechaInicio;
+    }
+
+    public abstract double CalcularDuracionEstimada();
+
+    public abstract void MostrarDetalles();
+}
